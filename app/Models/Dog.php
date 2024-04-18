6f74842bce2f7a32a8ac07fbf29b761c731ae658
sgledhill1695 <samgledhill1695@hotmail.com>
2024-04-18 15:13:13 +0100

    first commit

---
+++ app/Models/Dog.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dog extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'allergies' => 'array',
+    ];
+}
